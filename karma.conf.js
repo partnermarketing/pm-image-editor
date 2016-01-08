2a2b2da2a4de7c3ae5309050cc0976564ed1c710
Anton Vasyliev <antonvas@gmail.com>
2016-01-08 14:14:07 +0000

    Support stuffs

---
+++ karma.conf.js
@@ -0,0 +1,42 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/js/init.js',
+      'src/js/classes/draggable.js',
+      'src/js/classes/resizable.js',
+      'src/js/classes/image-selection.js',
+      'src/js/pm-image-editor.js',
+      'spec/**/*.js'
+    ],
+
+    preprocessors: {
+      'src/js/**/*.js': 'coverage'
+    },   
+
+    singleRun: true, 
+    autoWatch : false,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['PhantomJS'],
+
+    plugins : [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-coverage',
+            'karma-junit-reporter'
+    ],
+
+    reporters: ['dots', 'junit', 'coverage'],
+
+    junitReporter : {
+      outputFile: 'spec/unit.xml',
+      suite: 'unit'
+    }
+  });
+};
--- src/js/init.js
+++ src/js/init.js
@@ -2,5 +2,6 @@
     'use strict';
 
     angular.module('pmImageCrop', []);
+    angular.module('pmImageEditor', []);
 }());
 
